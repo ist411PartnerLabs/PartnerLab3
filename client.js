b9229b9892bee4ce70536e8870fa4a97fbbb9e1b
Ethan Niemeyer <ejn5196@psu.edu>
2022-04-28 14:02:30 -0400

    Commit

--- client.js
+++ client.js
@@ -2,10 +2,12 @@
 
 function createUsername()
 {
+    var userName = document.getElementById("createUsernameBox").value;
     
 }
 
 function login()
 {
+    var userName = document.getElementById("usernameBox").value;
 
 }
--- server.js
+++ server.js
@@ -1,6 +1,33 @@
+const fs = require('fs');
 const express = require('express');
-const app = express();
-const port = 3000;
+var app = express();
+app.listen(3000);
+
+
+
+
+
+
+const userList = [];
+
+fs.writeFile('userList.json', JSON.stringify(userList), err => {
+    if (err) throw err;
+    console.log('Saved');
+});
+
+
+function sayHello(yourName)
+{
+    console.log(`Hello there, ${yourName}`);
+
+    let outputField = document.querySelector("#outputField");
+
+    let name = document.querySelector("#nameBox").value;
+
+    outputField.innerHTML = `Hello there, ${name}!`
+    
+}
+
 
 
 
